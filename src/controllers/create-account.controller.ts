edc59ef9782b38cfefcb5335eb44e8678bc2b0bd
Yale Araujo <yale850@gmail.com>
2025-02-21 13:35:14 -0300

    feat: implement user account creation with validation and email uniqueness

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createAccountBodySchemma = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchemma = z.infer<typeof createAccountBodySchemma>
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prismaService: PrismaService) {
+    this.prismaService = prismaService
+  }
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchemma))
+  async handle(@Body() body: CreateAccountBodySchemma) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same email address already exists.',
+      )
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
