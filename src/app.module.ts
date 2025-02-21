baef75c3ea60e0cb9bb7e47020a1f24a3c3b2058
Yale Araujo <yale850@gmail.com>
2025-02-21 14:21:41 -0300

    feat: integrate ConfigService for environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
