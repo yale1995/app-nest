890454e438adb387d26db3012d251a6c4697ead5
Yale Araujo <yale850@gmail.com>
2025-02-27 15:11:12 -0300

    feat: update fetch recent questions to return 20 questions per page

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -24,7 +24,7 @@ export class FetchRecentQuestionsController {
 
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParam) {
-    const perPage = 1
+    const perPage = 20
 
     const questions = await this.prismaService.question.findMany({
       orderBy: {
