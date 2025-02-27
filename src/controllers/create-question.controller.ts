0bde11c54a77fa99eaabfbe252e117fa699dc1b7
Yale Araujo <yale850@gmail.com>
2025-02-27 10:45:36 -0300

    feat: implement question creation with authenticated user and input validation

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,14 +1,55 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { UserPayload } from 'src/auth/jwt.strategy'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBody = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor(private prismaService: PrismaService) {
     this.prismaService = prismaService
   }
 
   @Post()
-  async handle() {}
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(bodyValidationPipe) body: CreateQuestionBody,
+  ) {
+    const { title, content } = body
+
+    const userId = user.sub
+
+    const slug = this.convertioStug(title)
+
+    await this.prismaService.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  private convertioStug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u836f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
+  }
 }
